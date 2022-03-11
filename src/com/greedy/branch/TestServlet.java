024057abb22bc6b15d80fdda3b7c7e3d69b42f7f
byeol <byeol@byeol>
2022-03-11 12:30:03 +0900

    first commit

---
+++ src/com/greedy/branch/TestServlet.java
@@ -0,0 +1,21 @@
+package com.greedy.branch;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("test", "git branch test입니다.");
+		request.getRequestDispatcher("/WEB-INF/views/branch/result.jsp").forward(request, response);
+	}
+
+	
+
+}
